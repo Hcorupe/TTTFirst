306362551b32f635df4fbff7a0143f68906e4240
Duenas12b <Duenas.g.andy@gmail.com>
2020-02-10 17:42:55 -0800

    Made onClicked Board UI functions more efficient

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,11 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
-
-import java.awt.*;
-
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import javax.swing.*;
-
 public class Controller {
-
-    int controlLoop = 0; //Adds 1 number per Button Click.. 0 % 2 = 0 -> (X) , 1 % 2 = 1 -> (O), 2 % 2 = 0 -> (X)
-    // 3 % 2 = 1 -> (O), etc....
-    int turn = 0;        //Determines who's turn it is by using control loop. (X or O)
 
     /*              GAME BOARD
      *              |       |
@@ -27,187 +18,39 @@ public class Controller {
      *      twoZero |twoOne | twoTwo
      *              |       |
      */
+
     @FXML
-    Label zeroZero;
+    Button zeroZero;
     @FXML
-    Label zeroOne;
+    Button zeroOne;
     @FXML
-    Label zeroTwo;
+    Button zeroTwo;
     @FXML
-    Label oneZero;
+    Button oneZero;
     @FXML
-    Label oneOne;
+    Button oneOne;
     @FXML
-    Label OneTwo;
+    Button oneTwo;
     @FXML
-    Label twoZero;
+    Button twoZero;
     @FXML
-    Label twoOne;
+    Button twoOne;
     @FXML
-    Label twoTwo;
+    Button twoTwo;
 
-    public void onClicked(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            zeroZero.setPrefSize(200,200);
-            zeroZero.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            zeroZero.setPrefSize(200,200);
-            zeroZero.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
+    private boolean firstPlayer = true;
 
-        controlLoop++;
+    public void onClicked(ActionEvent Event) {
+        Button clickedButton = (Button) Event.getTarget();  //Stores the button being pressed
+        String buttonLabel = clickedButton.getText();       //Stores the button's Text (which starts off blank)
+
+        if ("".equals(buttonLabel) && firstPlayer){       //Checking if button Text is empty and if its player 1's turn
+            clickedButton.setText("X");                   //Makes the empty buttons Text into an "X"
+            firstPlayer = false;                          //Switches players turn
+        }
+        else if("".equals(buttonLabel) && !firstPlayer){  //Checking if button Text is empty and if its player 2's turn
+            clickedButton.setText("O");                   //Makes the empty buttons Text into an "O"
+            firstPlayer = true;                           //Switches players turn
+        }
     }
-    public void onClicked2(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            zeroOne.setPrefSize(200,200);
-            zeroOne.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            zeroOne.setPrefSize(200,200);
-            zeroOne.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked3(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            zeroTwo.setPrefSize(200,200);
-            zeroTwo.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            zeroTwo.setPrefSize(200,200);
-            zeroTwo.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked4(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            oneZero.setPrefSize(200,200);
-            oneZero.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            oneZero.setPrefSize(200,200);
-            oneZero.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked5(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            oneOne.setPrefSize(200,200);
-            oneOne.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            oneOne.setPrefSize(200,200);
-            oneOne.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked6(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            OneTwo.setPrefSize(200,200);
-            OneTwo.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            OneTwo.setPrefSize(200,200);
-            OneTwo.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked7(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            twoZero.setPrefSize(200,200);
-            twoZero.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            twoZero.setPrefSize(200,200);
-            twoZero.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked8(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            twoOne.setPrefSize(200,200);
-            twoOne.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            twoOne.setPrefSize(200,200);
-            twoOne.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-    public void onClicked9(){
-        turn = controlLoop % 2;
-        if(turn == 0){
-            System.out.println(controlLoop+"X");
-            twoTwo.setPrefSize(200,200);
-            twoTwo.setText("X");
-        }
-        else if(turn == 1){
-            System.out.println(controlLoop+"O");
-            twoTwo.setPrefSize(200,200);
-            twoTwo.setText("O");
-        }
-        else{
-            System.out.println(controlLoop+"SOMETHINGS WRONG");
-        }
-
-        controlLoop++;
-    }
-
-
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,17 +10,19 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Tic-Tac-Toe");
         primaryStage.setScene(new Scene(root, 600, 400));
         root.getStylesheets().add("test.css");
         //root.getStylesheets().add(getClass().getResource("test.css").toExternalForm());
         primaryStage.show();
-        //COMMENTS
+
     }
 
     public static void main(String[] args) {
         launch(args);
+
     }
+
 }
